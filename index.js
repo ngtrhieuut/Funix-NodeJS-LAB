1a9e274b0966fcdba720643bf180f181e772d81a
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-17 22:01:42 +0700

    LAB1.3

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><head><title>My First Page</title></head><body><h1>Hello from my Node.js server!</h1></body></html>');
+    res.end();
 })
 
 server.listen(3000)
