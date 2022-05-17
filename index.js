bc8c119f2d75aa3b0a99b13f9e83ba1836ff4928
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-17 22:14:00 +0700

    LAB1.5

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
+  const url = req.url;
+  const method = req.method;
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
@@ -16,6 +23,6 @@ const server = http.createServer((req, res) => {
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
-})
+});
 
-server.listen(3000)
+server.listen(3000);
