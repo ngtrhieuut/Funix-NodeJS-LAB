51bda2798cb78e7e6915139ee2821856c16c37c1
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-19 22:39:19 +0700

    LAB2.4

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('123');
-    next();
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>The Add product page</h1>')
 })
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
     console.log('345');
     res.send('<h1>Hello from Express.js</h1>')
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
