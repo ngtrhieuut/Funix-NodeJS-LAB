6ab26085cdb42caf3b797bb859453716ae6cb925
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-19 23:30:12 +0700

    LAB2.7

--- index.js
+++ index.js
@@ -3,20 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRouters = require('./routes/admin')
+const shopRouters = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
-})
+app.use(adminRouters);
+app.use(shopRouters);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/');
-})
-
-app.use('/',(req, res, next) => {
-    console.log('345');
-    res.send('<h1>Hello from Express.js</h1>')
-})
 
 app.listen(3000);
