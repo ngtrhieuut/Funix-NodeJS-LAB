7961d41c8e555f34d8cc77db999d2249f22a3dcd
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-19 22:21:15 +0700

    LAB2.2

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('123');
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log('345');
+    
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);
