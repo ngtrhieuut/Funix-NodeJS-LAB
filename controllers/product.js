9c48c1ab35fcc4142a06e8d9177b9810758840c7
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-21 19:16:36 +0700

    LAB4.4

--- controllers/product.js
+++ controllers/product.js
@@ -1,30 +1,30 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
-        pageTitle: 'Add Product', 
-        path: '/admin/add-product',
-        formsCSS: true,
-        activeAddProduct: true,
-        productCSS: true
-    })
-}
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
-    console.log(product)
-    product.save;
-    res.redirect('/');
-}
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
+  Product.fetchAll(products => {
     res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProducts: products.length > 0, 
-        activeShop: true, 
-        productCSS: true,
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
     });
-}
+  });
+};
