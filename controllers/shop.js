f27eef313fc1b27eed1f7dbd49852ba66c71febf
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-23 20:42:12 +0700

    LAB4.20

--- controllers/shop.js
+++ controllers/shop.js
@@ -53,13 +53,20 @@ exports.getCart = (req, res, next) => {
   });
 };
 
-
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId, product => {
     Cart.addProduct(prodId, product.price)
   });
   res.redirect('/cart');
+}
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  })
 }
 
 exports.getOrders = (req, res, next) => {
