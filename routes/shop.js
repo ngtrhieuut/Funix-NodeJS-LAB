dd9e8bb3d53939555da1b34b0702a5cc43ff84f2
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-23 11:56:25 +0700

    LAB4.9

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/product/:productId', shopController.getProduct);
+router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
@@ -19,3 +19,5 @@ router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
+
+
