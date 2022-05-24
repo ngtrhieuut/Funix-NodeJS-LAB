6fbb96ffce148798adfcbff460e58da548155863
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-24 14:46:15 +0700

    LAB5.9

--- models/product.js
+++ models/product.js
@@ -1,31 +1,61 @@
-const db = require('../util/database');
+// const db = require('../util/database');
 
-const Cart = require('./cart');
+// const Cart = require('./cart');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+// module.exports = class Product {
+//   constructor(id, title, imageUrl, description, price) {
+//     this.id = id;
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.price = price;
+//   }
+
+//   save() {
+//     return db.execute(
+//       'INSERT INTO `node-complete`.`products` (`title`, `price`, `description`, `imageUrl`) VALUES (?, ?, ?, ?)',
+//       [this.title, this.price, this.description, this.imageUrl])
+//   }
+
+//   static deleteById(id) {
+
+//   }
+
+//   static fetchAll() {
+//     return db.execute('SELECT * FROM `node-complete`.products');
+//   }
+
+//   static findById(id) {
+//     return db.execute('SELECT * FROM `node-complete`.products WHERE products.id = ?', [id])
+//   }
+// };
+
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  save() {
-    return db.execute(
-      'INSERT INTO `node-complete`.`products` (`title`, `price`, `description`, `imageUrl`) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.description, this.imageUrl])
-  }
-
-  static deleteById(id) {
-
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM `node-complete`.products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM `node-complete`.products WHERE products.id = ?', [id])
-  }
-};
+module.exports = Product;
