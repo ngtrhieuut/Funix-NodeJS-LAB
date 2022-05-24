8ff1ac11ade4f6102c2a3bc62d42124a185b779f
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-24 19:14:42 +0700

    LAB5.18

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-const User = sequelize.defind('user', {
+const User = sequelize.define('user', {
     id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
