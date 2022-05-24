f468b0ceeb271015bcc86362d3324156bf4a65b5
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-24 14:38:32 +0700

    LAB5.8

--- util/database.js
+++ util/database.js
@@ -1,10 +1,16 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Alo113114115@@'
-});
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'Alo113114115@@'
+// });
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'Alo113114115@@', {dialect: 'mysql', host: 'localhost'});
+
+module.exports = sequelize;
