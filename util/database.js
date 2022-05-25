8010a055b1d312a37839cb4949f57b065d394988
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-25 19:40:51 +0700

    LAB6.1

--- util/database.js
+++ util/database.js
@@ -1,8 +1,25 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete3', 'root', 'Alo113114115@@', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+// const sequelize = new Sequelize('node-complete3', 'root', 'Alo113114115@@', {
+//   dialect: 'mysql',
+//   host: 'localhost'
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const MongoConnect = (callback) => {
+  
+  MongoClient.connect('mongodb+srv://ngtrhieuut:Alo113114115@ngtrhieuut.2ktcr.mongodb.net/?retryWrites=true&w=majority')
+    .then(result => {
+      console.log('Connected');
+      callback(result);
+    })
+    .catch(err => console.log(err));
+
+}
+
+module.exports = MongoConnect;
+
