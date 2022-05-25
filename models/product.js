12e06aa5d813514b7aa3123e68e7f44272e9cc6c
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-05-25 20:14:43 +0700

    LAB6.5

--- models/product.js
+++ models/product.js
@@ -13,7 +13,7 @@ class Product {
 
   save() {
     const db = getDb();
-    db.collection('products').insertOne(this)
+    return db.collection('products').insertOne(this)
       .then(result => {
         console.log(result);
       })
