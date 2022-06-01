85059e6478f38904580a6b69a9f860304177e89e
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-06-01 22:20:21 +0700

    LAB9.11

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator');
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -5,6 +6,9 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
+    hasError: false,
+    errorMessage: null,
+    validationErrors: []
   });
 };
 
@@ -13,6 +17,26 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/edit-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array()
+    });
+  }
+
   const product = new Product({
     title: title,
     price: price,
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,29 @@
 const path = require('path');
 
 const express = require('express');
+const { body } = require('express-validator');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 // /admin/add-product => GET
-router.get('/add-product', isAuth, adminController.getAddProduct);
+router.post(
+    '/add-product',
+    [
+      body('title')
+        .isString()
+        .isLength({ min: 3 })
+        .trim(),
+      body('imageUrl').isURL(),
+      body('price').isFloat(),
+      body('description')
+        .isLength({ min: 5, max: 400 })
+        .trim()
+    ],
+    isAuth,
+    adminController.postAddProduct
+  );
 
 // /admin/products => GET
 router.get('/products', isAuth, adminController.getProducts);
@@ -17,7 +33,22 @@ router.post('/add-product', isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post(
+    '/edit-product',
+    [
+      body('title')
+        .isString()
+        .isLength({ min: 3 })
+        .trim(),
+      body('imageUrl').isURL(),
+      body('price').isFloat(),
+      body('description')
+        .isLength({ min: 5, max: 400 })
+        .trim()
+    ],
+    isAuth,
+    adminController.postEditProduct
+  );
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
